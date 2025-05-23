abf756ea9f1c562a0d254972ccebd8242fad160f
Teoh Tuan Tian <73980560+teoh-radicals@users.noreply.github.com>
2025-05-23 17:41:55 +0800

    Update FlutterBroadcastsPlugin.kt

--- android/src/main/kotlin/de/kevlatus/flutter_broadcasts/FlutterBroadcastsPlugin.kt
+++ android/src/main/kotlin/de/kevlatus/flutter_broadcasts/FlutterBroadcastsPlugin.kt
@@ -13,6 +13,8 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import java.io.Serializable
+import android.os.Build
+import android.content.BroadcastOptions.RECEIVER_NOT_EXPORTED
 
 class CustomBroadcastReceiver(
         val id: Int,
